6c63ee806169ecb1ce214f7b425ad961a94b62e9
BrTrGr <bruno.triano@gmail.com>
2025-11-14 19:30:05 +0100

    some other functions

---
+++ main.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+
+# See your entire task list in a clean, scrollable frame.
+def view_all_tasks():
+    pass
+# Toggle a task status from "Pending" to "Completed".
+def mark_as_complete():
+    pass
