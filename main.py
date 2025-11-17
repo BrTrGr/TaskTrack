4d267078ace2cbf965e658d6b33dcf18c092c558
BrTrGr <bruno.triano@gmail.com>
2025-11-17 19:29:39 +0100

    clear main.py

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import customtkinter as ctk
 # import customtinker *
 
-# See your entire task list in a clean, scrollable frame.
-def view_all_tasks():
-    pass
-
-# Toggle a task status from "Pending" to "Completed".
-def mark_as_complete():
-    pass
-
-
-# Quickly add new tasks with a title and priority
-def add_task():
-    pass
-
-# Remove tasks you no longer need. 
-def delete_tasks():
-    pass
-
-# Assign priority levels to tasks
-def priority_level():
-    pass
 
     
 # git pull origin main
