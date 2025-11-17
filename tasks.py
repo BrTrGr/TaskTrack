d2f495ba8507fb4b803f3388c924a95cb458e093
BrTrGr <bruno.triano@gmail.com>
2025-11-17 19:28:35 +0100

    new document tasks.py

---
+++ tasks.py
@@ -0,0 +1,20 @@
+# See your entire task list in a clean, scrollable frame.
+def view_all_tasks():
+    pass
+
+# Toggle a task status from "Pending" to "Completed".
+def mark_as_complete():
+    pass
+
+
+# Quickly add new tasks with a title and priority
+def add_task():
+    pass
+
+# Remove tasks you no longer need. 
+def delete_tasks():
+    pass
+
+# Assign priority levels to tasks
+def priority_level():
+    pass
